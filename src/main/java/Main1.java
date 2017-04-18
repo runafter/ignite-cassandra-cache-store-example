35a20bbcca7181c166941072a40d73f8d5a30a8f
runafter <runafter82@gmail.com>
2017-04-18 19:35:12 +0900

    add indexed query cache test runner

--- src/main/java/Main1.java
+++ src/main/java/Main1.java
@@ -4,13 +4,13 @@
  * Created by runaf on 2017-04-18.
  */
 public class Main1 {
-    private static final int SIZE = 10000;
+    private static final int SIZE = 1000;
     private static final int ITEM_SIZE = 1000;
 
     public static void main(String[] args) {
 //        new CacheValueAsListTestRunner(SIZE, ITEM_SIZE).run();
-        new CacheKeyAsListTestRunner(SIZE, ITEM_SIZE).run();
-//        new CacheIndexQueryTestRunner(SIZE, ITEM_SIZE).run();
+//        new CacheKeyAsListTestRunner(SIZE, ITEM_SIZE).run();
+        new CacheIndexQueryTestRunner(SIZE, ITEM_SIZE).run();
     }
 
 
